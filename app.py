82895c51aaa1cda343efb5d4d62c5473f85beb27
palak-7117 <panwarrpalakk7@gmail.com>
2026-07-02 23:40:37 +0530

    updated app.py along with the requirements.txt

--- app.py
+++ app.py
@@ -169,4 +169,5 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
